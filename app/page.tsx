a8b4270782b516ab74a3a20147d09a80a4af8e69
natasyo <natasyo23@gmail.com>
2024-12-30 00:21:27 +0300

    init: discont

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Banner from '@/react/sections/main-page/banner';
 import CustomerSay from '@/react/sections/main-page/customer-say';
 import Dashboard from '@/react/sections/main-page/dashboard';
+import Discont from '@/react/sections/main-page/discont';
 import DownLoad from '@/react/sections/main-page/download';
 import Info from '@/react/sections/main-page/info';
 import Restaurants from '@/react/sections/main-page/restaurants';
@@ -16,6 +17,7 @@ export default function Home() {
       <TopDishes />
       <Dashboard />
       <CustomerSay />
+      <Discont />
     </div>
   );
 }
---
+++ react/sections/main-page/discont.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from 'react';
+
+const Discont: FunctionComponent = () => {
+  return (
+    <div>
+      <div className="container rounded-[30px] bg-[url('../public/img/discont-bg.png')] py-[36px]">
+        <p className="font-popins text-7xl font-extrabold uppercase text-white">GET 50%</p>
+      </div>
+    </div>
+  );
+};
+
+export default Discont;
