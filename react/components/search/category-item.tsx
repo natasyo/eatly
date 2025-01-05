87444351478800015bb4efc52d917fe68cea3d0b
natasyo <natasyo23@gmail.com>
2025-01-05 10:28:49 +0300

    feat: current item

--- react/components/search/category-item.tsx
+++ react/components/search/category-item.tsx
@@ -1,3 +1,4 @@
+import { nexToRGB } from '@/functions/nexToRgb';
 import { Category } from '@/types';
 import Image from 'next/image';
 import { FunctionComponent } from 'react';
@@ -6,18 +7,25 @@ interface CategoryItemProps {
   className?: string;
   item: Category;
   isSelect?: boolean;
-  onSelectCategory?: () => void;
+  onSelectCategory?: (category: Category) => void;
 }
 
 const CategoryItem: FunctionComponent<CategoryItemProps> = ({
   className,
   item,
+  isSelect,
   onSelectCategory,
 }) => {
   return (
     <div
-      className={`${className ? className : ''} flex flex-col items-center rounded-xl pb-3 pt-2`}
-      style={{ background: item.colorBg }}
+      className={`${className ? className : ''} flex flex-col items-center rounded-xl bg-opacity-[0.35] pb-3 pt-2`}
+      style={{
+        background: nexToRGB(item.colorBg, 0.35),
+        border: `${isSelect ? '3px solid ' + item.colorBg : 'none'}`,
+      }}
+      onClick={() => {
+        onSelectCategory && onSelectCategory(item);
+      }}
     >
       <div className="mb-3 flex h-[37px] w-full items-center justify-center">
         <Image src={item.image} alt={item.title} width={32} height={37} className="max-h-[37px]" />
--- react/components/search/category-search.tsx
+++ react/components/search/category-search.tsx
@@ -11,17 +11,29 @@ interface CategorySearchProps {
 
 const CategorySearch: FunctionComponent<CategorySearchProps> = ({ className }) => {
   const categoriesdata = getCategories();
+  const [currentCategory, setCurrentCategory] = useState<Category>();
   const [categories, setCategories] = useState<Category[]>(categoriesdata);
   useEffect(() => {
     setCategories(categoriesdata);
   }, [categoriesdata]);
   useEffect(() => {});
+  function selectCategory(item: Category) {
+    console.log(item);
+    setCurrentCategory(item);
+  }
   return (
     <div className={`${className ? className : ''}`}>
       <p className="mb-[18px] font-popins text-lg font-semibold">Category</p>
       <div className="grid grid-cols-4 gap-[10px]">
         {categories ? (
-          categories.map((item) => <CategoryItem key={item.id} item={item} />)
+          categories.map((item) => (
+            <CategoryItem
+              key={item.id}
+              item={item}
+              onSelectCategory={selectCategory}
+              isSelect={item.id === currentCategory?.id}
+            />
+          ))
         ) : (
           <Loading />
         )}
