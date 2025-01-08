cdc06071a30f76860dbe0f6925e02b3faf1a15f6
natasyo <natasyo23@gmail.com>
2025-01-08 23:07:04 +0300

    fix: menu

--- react/sections/menu-page/search.tsx
+++ react/sections/menu-page/search.tsx
@@ -13,7 +13,7 @@ interface SearchProps {
 const Search: FunctionComponent<SearchProps> = ({ className }) => {
   return (
     <div
-      className={`container ${className ? className : ''} :grid-cols-[minmax(578px,_1fr)_268px] md:grid md:grid-cols-[minmax(578px,_1fr)_268px] md:gap-x-10 md:pt-20 xl:grid-cols-[minmax(758px,_1fr)_377px] xl:gap-x-[60px] xl:pt-[120px]`}
+      className={`container ${className ? className : ''} :grid-cols-[minmax(578px,_1fr)_268px] md:grid md:grid-cols-[minmax(578px,_1fr)_268px] md:gap-x-10 md:pt-20 xl:grid-cols-[minmax(758px,_1fr)_377px] xl:gap-x-[60px] xl:pt-[120px] 2xl:grid-cols-[minmax(758px,_1fr)_450px] 2xl:gap-x-20`}
     >
       <StocksCarousel className="-mr-4 mb-[73px] mt-[60px] max-w-full md:-mt-2 md:mb-16 xl:mb-21" />
       <CategorySearch className="row-span-2 mb-[82px] rounded-[20px] bg-white p-[26px] md:mb-0 md:pb-7" />
