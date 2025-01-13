b8832bec65223037f385406e297ae6123f82cc8f
natasyo <natasyo23@gmail.com>
2025-01-13 14:06:41 +0300

    feat:questions

--- react/components/questions/question-item.tsx
+++ react/components/questions/question-item.tsx
@@ -16,20 +16,24 @@ const QuestionItem: FunctionComponent<QuestionItemProps> = ({
 }) => {
   return (
     <div
-      className={`${className ? className : ''} border-b border-eatly-gray-800 leading-[258%] lg:pb-4 lg:pt-7`}
+      className={`${className ? className : ''} flex items-start justify-between border-b border-eatly-gray-800 pb-2 pt-4 leading-[258%] md:pb-3 md:pt-5 lg:pb-4 lg:pt-7`}
+      onClick={() => {
+        onShow && onShow(question);
+      }}
     >
-      <p
-        className="flex items-center justify-between font-popins text-xs font-semibold text-eatly-black-600 md:text-base lg:text-2xl"
-        onClick={() => {
-          onShow && onShow(question);
-        }}
-      >
-        <span>{question.question}</span>
+      <div className="max-w-[80%]">
+        <p
+          className={`font-popins text-xs font-semibold text-eatly-black-600 md:text-base lg:text-2xl ${isShow ? 'mb-1' : 'mb-0'}`}
+        >
+          <span>{question.question}</span>
+        </p>
+        <p className={`${isShow ? 'block' : 'hidden'}`}>{question.answer}</p>
+      </div>
+      <div>
         <span className="ml-10 flex size-4 items-center justify-center rounded-full bg-eatly-violet font-normal text-white md:size-[22px] lg:size-[30px]">
           {isShow ? '-' : '+'}
         </span>
-      </p>
-      <p className={`${isShow ? 'block' : 'hidden'}`}>{question.answer}</p>
+      </div>
     </div>
   );
 };
