0c82aaebc15513aa91fc1dea349d3e0d87eb38c9
natasyo <natasyo23@gmail.com>
2025-01-30 13:15:28 +0300

    feat: text box

--- app/admin/shop/types/page.tsx
+++ app/admin/shop/types/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ProductTypeController } from '@/controllers/prisma/product_type_controller';
+import TextBox from '@/react/components/fields/text-box';
 import { TypeDTO } from '@/types';
 import { ChangeEvent, FunctionComponent, useState } from 'react';
 
@@ -10,14 +11,15 @@ const TypesProductPage: FunctionComponent = () => {
   return (
     <div>
       <h2 className="text-center">Types</h2>
-      <input
+      {/* <input
         type="text"
         value={type?.title}
         onChange={(e: ChangeEvent) => {
           const title = (e.target as HTMLInputElement).value;
           setType({ ...type, title });
         }}
-      />
+      /> */}
+      <TextBox label="Type" />
       <input
         type="color"
         value={type?.bgColor}
---
+++ react/components/fields/text-box.tsx
@@ -0,0 +1,23 @@
+import { DetailedHTMLProps, FunctionComponent, InputHTMLAttributes } from 'react';
+
+interface TextBoxProps
+  extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+  label?: string;
+}
+
+const TextBox: FunctionComponent<TextBoxProps> = (props) => {
+  return (
+    <label className="mb-5 flex flex-col">
+      {props.label && (
+        <span className="mb-1 text-lg font-medium text-eatly-black-500">{props.label}</span>
+      )}
+      <input
+        {...props}
+        type="text"
+        className={`rounded-lg border-eatly-violet-500 p-3 text-lg text-eatly-black focus-visible:outline-eatly-violet ${props.className ? props.className : ''}`}
+      />
+    </label>
+  );
+};
+
+export default TextBox;
