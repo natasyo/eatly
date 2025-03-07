265e926c0c1b2c6352eb3186b82f30d46ae77dd5
natasyo <natasyo23@gmail.com>
2025-03-07 10:45:50 +0300

    comitfghdfgh

---
+++ app/auth/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+import Btn from '@/react/components/buttons/btn';
+import BtnPrimary from '@/react/components/buttons/btn-primary';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { FunctionComponent } from 'react';
+
+const Auth: FunctionComponent = () => {
+  const { data: session } = useSession();
+
+  return session ? (
+    <div className="container">
+      <p>Welcome, {session.user?.name}!</p>
+      <BtnPrimary onClick={() => signOut({ redirect: false })}>Sign out</BtnPrimary>
+    </div>
+  ) : (
+    <div className="container h-full content-center text-center">
+      <p className="text-xl font-bold text-eatly-black-100"> Sign in with Google</p>
+      <Btn onClick={() => signIn('google')}>
+        <svg className="icon" height={20} width={20}>
+          <use xlinkHref="img/sprite.svg#google"></use>
+        </svg>
+      </Btn>
+    </div>
+  );
+};
+export default Auth;
